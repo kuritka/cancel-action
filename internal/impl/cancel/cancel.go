d52e8163a01de9ec090e2b2acff9f522e735632b
kuritka <kuritka@gmail.com>
2022-01-28 10:27:36 +0100

    reque9st factory

--- internal/impl/cancel/cancel.go
+++ internal/impl/cancel/cancel.go
@@ -1,7 +1,6 @@
 package cancel
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"io/ioutil"
@@ -14,27 +13,30 @@ import (
 var logger = log.Log
 
 type Cancel struct {
-	o impl.ActionOpts
+	f *requestFactory
 }
 
 func NewCommand(o impl.ActionOpts) *Cancel {
 	return &Cancel{
-		o: o,
+		f: newRequestFactory(context.TODO(), o),
 	}
 }
 
 func (c *Cancel) Run() error {
 	logger.Info().Msg("Cancelling....")
-	result, status, err := request(context.TODO(), c.o.GitHub, http.MethodPost, "cancel")
+
+	status, err := request(c.f.getImpl(cancelWorkflow))
 	if err != nil {
-		logger.Err(err).Msgf("error during github request; code: %v; body: %s", status, result)
+		logger.Err(err).Msgf("error during github request; code: %v", status)
+		return err
 	}
 	logger.Info().Msgf("returned status code: %v", status)
 
 	logger.Info().Msg("Deleting....")
-	result, status, err = request(context.TODO(), c.o.GitHub, http.MethodDelete, "")
+	status, err = request(c.f.getImpl(deleteWorkflow))
 	if err != nil {
-		logger.Err(err).Msgf("error during github DELETE request; code: %v; body: %s", status, result)
+		logger.Err(err).Msgf("error during github DELETE request; code: %v", status)
+		return err
 	}
 	logger.Info().Msgf("returned status code: %v", status)
 
@@ -45,31 +47,25 @@ func (c *Cancel) String() string {
 	return "CANCEL ACTION"
 }
 
-func request(ctx context.Context, gh impl.GitHub, httpMethod, subaction string) (result string, status int, err error) {
-	json := new(bytes.Buffer)
-	var url = fmt.Sprintf("https://api.github.com/repos/%s/actions/runs/%s/%s", gh.Repository, gh.RunID, subaction)
-	logger.Debug().Msgf("hitting URL: %s", url)
-	req, err := http.NewRequestWithContext(ctx, httpMethod, url, json)
+func request(f func() (*http.Request, error)) (status int, err error) {
+	req, err := f()
 	if err != nil {
-		return "", 0, err
+		return 0, err
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "token "+gh.Token)
-	req.Header.Set("User-Agent", "actions/cancel-action")
-
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", resp.StatusCode, err
+		return resp.StatusCode, err
 	}
 	// nolint:errcheck
 	defer resp.Body.Close()
 	if resp.StatusCode < 200 || resp.StatusCode > 208 {
-		return "", resp.StatusCode, fmt.Errorf(resp.Status)
+		return resp.StatusCode, fmt.Errorf(resp.Status)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", resp.StatusCode, err
+		return resp.StatusCode, err
 	}
-	return string(body), resp.StatusCode, nil
+	logger.Debug().Msgf("response body: %s", string(body))
+	return resp.StatusCode, nil
 }
