d7a7697ad5e148c19e92dc0d54cd516521b68f98
kuritka <kuritka@gmail.com>
2022-01-28 09:38:25 +0100

    fix linting issues

--- internal/impl/cancel/cancel.go
+++ internal/impl/cancel/cancel.go
@@ -25,16 +25,16 @@ func NewCommand(o impl.ActionOpts) *Cancel {
 
 func (c *Cancel) Run() error {
 	logger.Info().Msg("Cancelling....")
-	_, status, err := request(context.TODO(), c.o.GitHub)
+	result, status, err := request(context.TODO(), c.o.GitHub, http.MethodPost, "cancel")
 	if err != nil {
-		logger.Err(err).Msg("error during github request")
+		logger.Err(err).Msgf("error during github request; code: %v; body: %s", status, result)
 	}
 	logger.Info().Msgf("returned status code: %v", status)
 
 	logger.Info().Msg("Deleting....")
-	_, status, err = requestDelete(context.TODO(), c.o.GitHub)
+	result, status, err = request(context.TODO(), c.o.GitHub, http.MethodDelete, "")
 	if err != nil {
-		logger.Err(err).Msg("error during github DELETE request")
+		logger.Err(err).Msgf("error during github DELETE request; code: %v; body: %s", status, result)
 	}
 	logger.Info().Msgf("returned status code: %v", status)
 
@@ -45,40 +45,11 @@ func (c *Cancel) String() string {
 	return "CANCEL ACTION"
 }
 
-func request(ctx context.Context, gh impl.GitHub) (result string, status int, err error) {
+func request(ctx context.Context, gh impl.GitHub, httpMethod, subaction string) (result string, status int, err error) {
 	json := new(bytes.Buffer)
-	var url = fmt.Sprintf("https://api.github.com/repos/%s/actions/runs/%s/cancel", gh.Repository, gh.RunID)
+	var url = fmt.Sprintf("https://api.github.com/repos/%s/actions/runs/%s/%s", gh.Repository, gh.RunID, subaction)
 	logger.Debug().Msgf("hitting URL: %s", url)
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, json)
-	if err != nil {
-		return "", 0, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "token "+gh.Token)
-	req.Header.Set("User-Agent", "actions/cancel-action")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", resp.StatusCode, err
-	}
-	// nolint:errcheck
-	defer resp.Body.Close()
-	if resp.StatusCode < 200 || resp.StatusCode > 208 {
-		return "", resp.StatusCode, fmt.Errorf(resp.Status)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return "", resp.StatusCode, err
-	}
-	return string(body), resp.StatusCode, nil
-}
-
-func requestDelete(ctx context.Context, gh impl.GitHub) (result string, status int, err error) {
-	json := new(bytes.Buffer)
-	var url = fmt.Sprintf("https://api.github.com/repos/%s/actions/runs/%s", gh.Repository, gh.RunID)
-	logger.Debug().Msgf("hitting URL: %s", url)
-	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, url, json)
+	req, err := http.NewRequestWithContext(ctx, httpMethod, url, json)
 	if err != nil {
 		return "", 0, err
 	}
